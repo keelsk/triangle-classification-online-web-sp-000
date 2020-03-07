0db53dbecc9cab9fbe576f01c5356f1c4027812e
Kirsten Keels <keelsk@gmail.com>
2020-03-07 16:47:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  def initialize(side1:, side2:, side3:)
+    @sides = [@side1, @side2, @side3]
+  end
+  
+  def kind
+    if (@side3 > @side2 + @side1 || @side3 > @side2 + @side1 || @side3 > @side2 + @side1 || @side1 = 0 || @side2 = 0 || @side3 = 0)
+    else
+    
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle. Please try again"
+    end
+  end
 end
+
+
+
